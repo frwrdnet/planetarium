d3f05efa2211316bc78ea998e9e80e13f9fc8f17
Victor Zambrano <victor@frwrd.net>
2018-06-15 23:45:42 -0700

    planets rotate and traslate! earth looks like the moon, dunno why...

---
+++ planetarium/Planet.swift
@@ -0,0 +1,28 @@
+//
+//  Planet.swift
+//  planetarium
+//
+//  Created by Victor Zambrano on 6/3/18.
+//  Copyright © 2018 Victor Zambrano. All rights reserved.
+//
+
+//import Foundation
+//import SceneKit
+//
+//class Planet: SCNNode {
+//	
+//	var planetTexture: UIImage?
+//	var planetRradius: Float = 0.2
+//	var planetPosition: SCNVector3?
+//	
+//	override init() {
+//		super.init()
+//		self.geometry = SCNSphere(radius: 0.2)
+//		self.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/8k/8k_sun.jpg")
+//		//self.position = planetPosition? ?? SCNVector3(x: 0.0, y:0.0, z:0.0)
+//	}
+//	
+//	required init?(coder aDecoder: NSCoder) {
+//		super.init(coder: aDecoder)
+//	}
+//}
--- planetarium/ViewController.swift
+++ planetarium/ViewController.swift
@@ -13,6 +13,56 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+	
+	// MARK: - Planets
+	
+	/*
+	Relative sizes and distances:
+	Sun: 1,377,648 kilometers (Ratio: 1:1)
+	
+	Mercury: 4,880 kilometers (Ratio: 1:277)
+	Venus: 12,104 kilometers (Ratio: 1:113)
+	Earth: 12,756 kilometers (Ratio: 1:108)
+	Mars: 6,788 kilometers (Ratio: 1:208)
+	Jupiter: 142,740 kilometers (Ratio: 1:9.68)
+	Saturn: 120,034 kilometers (Ratio: 1:11.4)
+	Uranus: 51,152 kilometers (Ratio: 1:26.8)
+	Neptune: 49,620 kilometers (Ratio: 1:27.7)
+	*/
+	
+	struct Planet {
+		var name: String = "sun"
+		var distance: Float = 0.0
+		var radius: CGFloat = 0.2
+		var image: String = "art.scnassets/2k/2k_sun.jpg"
+		var duration: Double = 14.0
+		var orbit: Double = 14.0
+	}
+	
+	/*
+	Planet(name: "sun", distance: -1.1, radius: 0.2, image: UIImage(named: "art.scnassets/2k/2k_sun.jpg")!, duration: 14.0, orbit: 14.0),
+	Planet(name: "mercury", distance: -0.513, radius: 0.006, image: UIImage(named: "art.scnassets/2k/2k_mercury.jpg")!, duration: 14.0, orbit: 14.0),
+	Planet(name: "venus", distance: -0.178, radius: 0.015, image: UIImage(named: "art.scnassets/2k/2k_venus_atmosphere.jpg")!, duration: 14.0, orbit: 14.0),
+	Planet(name: "earth", distance: -0.1, radius: 0.016, image: UIImage(named: "art.scnassets/2k/2k_earth_daymap.jpg")!, duration: 14.0, orbit: 14.0),
+	Planet(name: "mars", distance: 1.62, radius: 0.008, image: UIImage(named: "art.scnassets/2k/2k_mars.jpg")!, duration: 14.0, orbit: 14.0),
+	Planet(name: "jupiter", distance: 5.30, radius: 0.175, image: UIImage(named: "art.scnassets/2k/2k_jupiter.jpg")!, duration: 14.0, orbit: 14.0),
+	Planet(name: "saturn", distance: 9.68, radius: 0.147, image: UIImage(named: "art.scnassets/2k/2k_saturn.jpg")!, duration: 14.0, orbit: 14.0),
+	Planet(name: "uranus", distance: 19.3, radius: 0.062, image: UIImage(named: "art.scnassets/2k/2k_uranus.jpg")!, duration: 14.0, orbit: 14.0),
+	Planet(name: "neptune", distance: 30.2, radius: 0.06, image: UIImage(named: "art.scnassets/2k/2k_neptune.jpg")!, duration: 14.0, orbit: 14.0),
+	*/
+	
+	let planets: [Planet] = [
+		Planet(name: "sun", distance: 0.0, radius: 0.2, image: "art.scnassets/2k/2k_sun.jpg", duration: 14.0, orbit: 14.0),
+		Planet(name: "mercury", distance: 0.4, radius: 0.006, image: "art.scnassets/2k/2k_mercury.jpg", duration: 14.0, orbit: 14.0),
+		Planet(name: "venus", distance: 0.7, radius: 0.015, image: "art.scnassets/2k/2k_venus_atmosphere.jpg", duration: 14.0, orbit: 14.0),
+		Planet(name: "earth", distance: 1.0, radius: 0.016, image: "art.scnassets/2k/2k_earth_day_map.jpg", duration: 14.0, orbit: 14.0),
+		//Planet(name: "earth", distance: 1.0, radius: 0.016, image: "art.scnassets/2k/2k_earth_nightmap.jpg", duration: 14.0, orbit: 14.0),
+		Planet(name: "mars", distance: 1.62, radius: 0.008, image: "art.scnassets/2k/2k_mars.jpg", duration: 14.0, orbit: 14.0),
+		Planet(name: "jupiter", distance: 5.30, radius: 0.175, image: "art.scnassets/2k/2k_jupiter.jpg", duration: 14.0, orbit: 14.0),
+		Planet(name: "saturn", distance: 9.68, radius: 0.147, image: "art.scnassets/2k/2k_saturn.jpg", duration: 14.0, orbit: 14.0),
+		Planet(name: "uranus", distance: 19.3, radius: 0.062, image: "art.scnassets/2k/2k_uranus.jpg", duration: 14.0, orbit: 14.0),
+		Planet(name: "neptune", distance: 30.2, radius: 0.06, image: "art.scnassets/2k/2k_neptune.jpg", duration: 14.0, orbit: 14.0),
+		]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,141 +73,73 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
 		
-		// MARK: - Planets
-		
-		/*
-		Relative sizes and distances:
-		Sun: 1,377,648 kilometers (Ratio: 1:1)
-		
-		Mercury: 4,880 kilometers (Ratio: 1:277)
-		Venus: 12,104 kilometers (Ratio: 1:113)
-		Earth: 12,756 kilometers (Ratio: 1:108)
-		Mars: 6,788 kilometers (Ratio: 1:208)
-		Jupiter: 142,740 kilometers (Ratio: 1:9.68)
-		Saturn: 120,034 kilometers (Ratio: 1:11.4)
-		Uranus: 51,152 kilometers (Ratio: 1:26.8)
-		Neptune: 49,620 kilometers (Ratio: 1:27.7)
-		*/
-		
-		// Sun
-		let sunSphere = SCNSphere(radius: 0.2)
-		let sunMaterial = SCNMaterial()
-		sunMaterial.diffuse.contents = UIImage(named: "art.scnassets/8k/8k_sun.jpg")
-		sunSphere.materials = [sunMaterial]
-		
-		let sunNode = SCNNode()
-		sunNode.position = SCNVector3(x: 0, y: 0, z: -1.1)
-		sunNode.geometry = sunSphere
-		
-		sceneView.scene.rootNode.addChildNode(sunNode)
-		
-		// Mercury
-		let mercurySphere = SCNSphere(radius: 0.006)
-		let mercuryMaterial = SCNMaterial()
-		mercuryMaterial.diffuse.contents = UIImage(named: "art.scnassets/8k/8k_mercury.jpg")
-		mercurySphere.materials = [mercuryMaterial]
-		
-		let mercuryNode = SCNNode()
-		mercuryNode.position = SCNVector3(x: 0, y: 0, z: -1*(1-0.487))
-		mercuryNode.geometry = mercurySphere
-		
-		sceneView.scene.rootNode.addChildNode(mercuryNode)
-		
-		// Venus
-		let venusSphere = SCNSphere(radius: 0.015)
-		let venusMaterial = SCNMaterial()
-		venusMaterial.diffuse.contents = UIImage(named: "art.scnassets/8k/8k_venus_atmosphere.jpg")
-		venusSphere.materials = [venusMaterial]
-		
-		let venusNode = SCNNode()
-		venusNode.position = SCNVector3(x: 0, y: 0, z: -1*(1-0.822))
-		venusNode.geometry = venusSphere
-		
-		sceneView.scene.rootNode.addChildNode(venusNode)
-		
-		// Earth
-		let earthSphere = SCNSphere(radius: 0.016)
-		let earthMaterial = SCNMaterial()
-		earthMaterial.diffuse.contents = UIImage(named: "art.scnassets/8k/8k_earth_daymap.jpg")
-		earthSphere.materials = [earthMaterial]
-		
-		let earthNode = SCNNode()
-		earthNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-		earthNode.geometry = earthSphere
-		
-		sceneView.scene.rootNode.addChildNode(earthNode)
-		
-		// Mars
-		let marsSphere = SCNSphere(radius: 0.008)
-		let marsMaterial = SCNMaterial()
-		marsMaterial.diffuse.contents = UIImage(named: "art.scnassets/8k/8k_mars.jpg")
-		marsSphere.materials = [marsMaterial]
-		
-		let marsNode = SCNNode()
-		marsNode.position = SCNVector3(x: 0, y: 0, z: 1.62)
-		marsNode.geometry = marsSphere
-		
-		sceneView.scene.rootNode.addChildNode(marsNode)
-		
-		// Jupiter
-		let jupiterSphere = SCNSphere(radius: 0.175)
-		let jupiterMaterial = SCNMaterial()
-		jupiterMaterial.diffuse.contents = UIImage(named: "art.scnassets/8k/8k_jupiter.jpg")
-		jupiterSphere.materials = [jupiterMaterial]
-		
-		let jupiterNode = SCNNode()
-		jupiterNode.position = SCNVector3(x: 0, y: 0, z: 5.30)
-		jupiterNode.geometry = jupiterSphere
-		
-		sceneView.scene.rootNode.addChildNode(jupiterNode)
-		
-		// Saturn
-		let saturnSphere = SCNSphere(radius: 0.147)
-		let saturnMaterial = SCNMaterial()
-		saturnMaterial.diffuse.contents = UIImage(named: "art.scnassets/8k/8k_saturn.jpg")
-		saturnSphere.materials = [saturnMaterial]
-		
-		let saturnNode = SCNNode()
-		saturnNode.position = SCNVector3(x: 0, y: 0, z: 9.68)
-		saturnNode.geometry = saturnSphere
-		
-		sceneView.scene.rootNode.addChildNode(saturnNode)
-		
-		// Uranus
-		let uranusSphere = SCNSphere(radius: 0.062)
-		let uranusMaterial = SCNMaterial()
-		uranusMaterial.diffuse.contents = UIImage(named: "art.scnassets/2k/2k_uranus.jpg")
-		uranusSphere.materials = [uranusMaterial]
-		
-		let uranusNode = SCNNode()
-		uranusNode.position = SCNVector3(x: 0, y: 0, z: 19.3)
-		uranusNode.geometry = uranusSphere
-		
-		sceneView.scene.rootNode.addChildNode(uranusNode)
-		
-		// Neptune
-		let neptuneSphere = SCNSphere(radius: 0.06)
-		let neptuneMaterial = SCNMaterial()
-		neptuneMaterial.diffuse.contents = UIImage(named: "art.scnassets/2k/2k_neptune.jpg")
-		neptuneSphere.materials = [neptuneMaterial]
-		
-		let neptuneNode = SCNNode()
-		neptuneNode.position = SCNVector3(x: 0, y: 0, z: 30.2)
-		neptuneNode.geometry = neptuneSphere
-		
-		sceneView.scene.rootNode.addChildNode(neptuneNode)
-		
-		
 		// Lighting
 		sceneView.autoenablesDefaultLighting = true
 		
-        
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
+		// Camera control
+		sceneView.allowsCameraControl = true
+
+		arrangePlanets()
+	
     }
+	
+	func arrangePlanets() {
+		for planet in planets {
+			print("----\nplanet.name: \(planet.name)")
+			print("planet.distance: \(planet.distance)")
+			print("planet.radius: \(planet.radius)")
+			print("planet.image: \(planet.image)")
+			print("planet.duration: \(planet.duration)")
+			print("planet.orbit: \(planet.orbit)")
+			
+			let sphere = SCNSphere(radius: planet.radius)
+			let material = SCNMaterial()
+			material.diffuse.contents = UIImage(named: planet.image)
+			sphere.materials = [material]
+			
+			let node = SCNNode()
+			let pos = planet.distance + Float(-1.1)
+			node.position = SCNVector3(x: 0, y: 0, z: pos)
+			print("node.position: \(node.position)")
+			node.geometry = sphere
+			
+			let helperNode = SCNNode()
+			helperNode.position = SCNVector3(x: 0, y: 0, z: -1.1)
+			print("helperNode.position: \(helperNode.position)")
+			helperNode.addChildNode(node)
+			
+			node.addAnimation(rotate(duration: planet.duration), forKey: "planetSpin")
+			helperNode.addAnimation(rotate(duration: planet.orbit), forKey: "planetOrbit")
+			
+			if planet.name == "sun" {
+				sceneView.scene.rootNode.addChildNode(node)
+			} else {
+				sceneView.scene.rootNode.addChildNode(helperNode)
+			}
+		}
+	}
+	
+	
+	// Animations
+	func rotate(duration: Double) -> CABasicAnimation {
+		let spin = CABasicAnimation(keyPath: "rotation")
+		// Use from-to to explicitly make a full rotation around z
+		spin.fromValue = NSValue(scnVector4: SCNVector4(x: 0, y: 1, z: 0, w: 0))
+		spin.toValue = NSValue(scnVector4: SCNVector4(x: 0, y: 1, z: 0, w: Float(CGFloat(2 * Double.pi))))
+		spin.duration = duration
+		spin.repeatCount = .infinity
+		return spin
+	}
+	
+	// traslate node
+	func orbit(radius: Float, duration: Double) -> CABasicAnimation {
+		let traslate = CABasicAnimation(keyPath: "rotation")
+		traslate.fromValue = NSValue(scnVector4: SCNVector4(x: 0, y: 1, z: 0, w: 0))
+		traslate.toValue = NSValue(scnVector4: SCNVector4(x: 0, y: 1, z: 0, w: Float(CGFloat(2 * Double.pi))))
+		traslate.duration = duration
+		traslate.repeatCount = .infinity
+		return traslate
+	}
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
